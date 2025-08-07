6496f2f59ed3c3c753c964474e082040e76b3bbc
Carl Dietz <s3668498@student.rmit.edu.au>
2025-08-07 18:56:35 +1000

    Model View Projection matrices

--- main.cpp
+++ main.cpp
@@ -192,6 +192,27 @@ int main()
     {
         processInput(window);
 
+        //model matrix
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+        //view
+        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        //projection
+        float aspectRatio = 800.0f / 600.0f;
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), aspectRatio, 0.1f, 100.0f);
+        
+        //model view projection
+        glm::mat4 mvp = projection * view * model;
+        
+        //send to shaders
+        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         //render here
         glClearColor(0.1f, 0.4f, 0.3f, 1.0f);
